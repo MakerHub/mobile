d3e3dd6a6f52f1dac1a88397cc44b177eb1094b4
Sam Kristoff <labviewhacker@gmail.com>
2015-02-15 15:22:55 -0600

    Added basic working Angular Cordova Project

---
+++ MakerHub Mobile/MakerHub Mobile/scripts/makerHubMobile.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var app = angular.module('makerHubMobile', ['ngCordova']);
+
+    app.controller('ButtonController', function () {
+        this.value = 0;
+        this.increment = function () {
+            this.value++;
+        };
+    });
+
+})();
